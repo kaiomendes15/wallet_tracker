2a0f961af95429a371e0bb7cc1df662e61aee264
Kaio Mendes <kakacnmendes@gmail.com>
2025-12-24 13:51:16 -0300

    inicializando projeto

---
+++ src/test/java/br/com/mywallet/app/MywalletApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mywallet.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
