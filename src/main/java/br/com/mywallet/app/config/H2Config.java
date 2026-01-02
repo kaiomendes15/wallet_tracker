5195b15b52fd94bc9ec0c861f6edd20c2b3384c7
kaiomendes15 <kakacnmendes@gmail.com>
2026-01-02 19:09:11 -0300

    configura conexao com h2

---
+++ src/main/java/br/com/mywallet/app/config/H2Config.java
@@ -0,0 +1,18 @@
+package br.com.mywallet.app.config; // Ajuste para seu pacote correto se necessário
+
+import org.h2.server.web.JakartaWebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class H2Config {
+
+    @Bean
+    public ServletRegistrationBean<JakartaWebServlet> h2servletRegistration() {
+        // Mudamos de new WebServlet() para new JakartaWebServlet()
+        ServletRegistrationBean<JakartaWebServlet> registrationBean = new ServletRegistrationBean<>(new JakartaWebServlet());
+        registrationBean.addUrlMappings("/h2-console/*");
+        return registrationBean;
+    }
+}
