799650cc2c127f5fa74cd0828369951053914e62
kaiomendes15 <kakacnmendes@gmail.com>
2026-01-03 11:15:14 -0300

    Adiciona o modelo da categoria

--- src/main/java/br/com/mywallet/app/domain/model/Categoria.java
+++ src/main/java/br/com/mywallet/app/domain/model/Categoria.java
@@ -1,4 +1,22 @@
 package br.com.mywallet.app.domain.model;
 
+import br.com.mywallet.app.domain.model.Usuario.Usuario;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
 public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id") // Define o nome da coluna no banco (FK)
+    private Usuario usuario;
+
+    @Column(nullable = false)
+    private String titulo;
 }
