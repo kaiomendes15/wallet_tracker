55ee49eed38dee10bb83d36e0c6d278dffc88e93
kaiomendes15 <kakacnmendes@gmail.com>
2026-01-03 11:38:12 -0300

    configura o ambiente

--- src/main/java/br/com/mywallet/app/domain/model/Transacao/Transacao.java
+++ src/main/java/br/com/mywallet/app/domain/model/Transacao/Transacao.java
@@ -1,4 +1,12 @@
 package br.com.mywallet.app.domain.model.Transacao;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class Transacao {
+    @Id
+    private Long id;
 }
---
+++ src/main/java/br/com/mywallet/app/domain/model/enums/TipoTransacao.java
@@ -0,0 +1,4 @@
+package br.com.mywallet.app.domain.model.enums;
+
+public enum TipoTransacao {
+}
