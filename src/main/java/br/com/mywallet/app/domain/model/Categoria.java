e426ff4c8a39f0942608950b44bd8b6e0fc76b4a
kaiomendes15 <kakacnmendes@gmail.com>
2026-01-03 11:38:59 -0300

    adiciona os models Categoria, Usuario e Transacao

--- src/main/java/br/com/mywallet/app/domain/model/Categoria.java
+++ src/main/java/br/com/mywallet/app/domain/model/Categoria.java
@@ -2,19 +2,21 @@ package br.com.mywallet.app.domain.model;
 
 import br.com.mywallet.app.domain.model.Usuario.Usuario;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "usuario_id") // Define o nome da coluna no banco (FK)
+    @JoinColumn(name = "usuario_id", nullable = false) // Define o nome da coluna no banco (FK)
     private Usuario usuario;
 
     @Column(nullable = false)
--- src/main/java/br/com/mywallet/app/domain/model/Usuario/Usuario.java
+++ src/main/java/br/com/mywallet/app/domain/model/Usuario/Usuario.java
@@ -3,10 +3,14 @@ package br.com.mywallet.app.domain.model.Usuario;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
 
     @Id
