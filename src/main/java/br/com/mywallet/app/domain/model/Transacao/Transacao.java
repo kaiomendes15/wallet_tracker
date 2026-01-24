55fd88e53908891fc41ab1d76976054e3fe41074
kaiomendes15 <kakacnmendes@gmail.com>
2026-01-23 22:01:47 -0300

    atualizacao da classe de transacao

--- src/main/java/br/com/mywallet/app/domain/model/Transacao/Transacao.java
+++ src/main/java/br/com/mywallet/app/domain/model/Transacao/Transacao.java
@@ -4,6 +4,7 @@ import br.com.mywallet.app.domain.model.Categoria.Categoria;
 import br.com.mywallet.app.domain.model.Usuario.Usuario;
 import br.com.mywallet.app.domain.enums.TipoTransacao;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Positive;
@@ -20,15 +21,16 @@ import java.time.LocalDate;
 public class Transacao {
     @Id
     private Long id;
-    @NotNull(message = "A descrição é obrigatória") // NotBlank se for String
+    @NotBlank(message = "A descrição é obrigatória.") // NotBlank se for String
     private String descricao;
-    @NotNull(message = "O valor é obrigatório")
-    @Positive(message = "O valor deve ser positivo")
+    @NotNull(message = "O valor é obrigatório.")
+    @Positive(message = "O valor deve ser positivo.")
     private Double valor;
-    @NotNull(message = "A data é obrigatória")
-    @PastOrPresent(message = "A data não pode ser futura") // Regra de negócio via Bean Validation
+    @NotNull(message = "A data é obrigatória.")
+    @PastOrPresent(message = "A data não pode ser futura.") // Regra de negócio via Bean Validation
     private LocalDate data;
-    @NotNull(message = "O tipo da transação é obrigatório")
+    @Positive(message = "Valor da transação não pode ser menor que zero.")
+    @NotNull(message = "O tipo da transação é obrigatório.")
     @Enumerated(EnumType.STRING) // Grava "RECEITA" no banco em vez de 0 ou 1
     private TipoTransacao tipo;
 
