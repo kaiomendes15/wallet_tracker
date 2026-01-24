6ca6e3c40d5e5b1f0777cb85294f6a997851395e
kaiomendes15 <kakacnmendes@gmail.com>
2026-01-24 12:09:55 -0300

    Adiciona o metodo para criar uma nova transacao

--- src/main/java/br/com/mywallet/app/domain/service/TransacaoService.java
+++ src/main/java/br/com/mywallet/app/domain/service/TransacaoService.java
@@ -1,11 +1,41 @@
 package br.com.mywallet.app.domain.service;
 
+import br.com.mywallet.app.domain.exceptions.ResourceNotFoundException;
+import br.com.mywallet.app.domain.model.Categoria.Categoria;
+import br.com.mywallet.app.domain.model.Transacao.Transacao;
+import br.com.mywallet.app.domain.model.Transacao.TransacaoRequestDTO;
+import br.com.mywallet.app.domain.model.Transacao.TransacaoResponseDTO;
+import br.com.mywallet.app.domain.model.Usuario.Usuario;
+import br.com.mywallet.app.repository.CategoriaRepository;
+import br.com.mywallet.app.repository.TransacaoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class TransacaoService {
 
-    public void criarTransacao() {
+    private final TransacaoRepository repository;
 
+    private final CategoriaRepository categoriaRepository;
+
+    public TransacaoResponseDTO criarTransacao(TransacaoRequestDTO data, Usuario usuarioLogado) {
+        Categoria categoria = categoriaRepository.findCategoriaByIdAndUsuarioId(data.categoriaId(), usuarioLogado.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("Categoria não encontrada para o usuário."));
+
+        Transacao novaTransacao = new Transacao(data, usuarioLogado, categoria);
+
+
+        Transacao transacaoSalva = repository.save(novaTransacao);
+
+        return new TransacaoResponseDTO(
+                transacaoSalva.getId(),
+                transacaoSalva.getData(),
+                transacaoSalva.getValor(),
+                transacaoSalva.getTipo()
+        );
     }
 }
