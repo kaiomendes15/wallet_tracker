76323cfc64997887f0be9e51646b4a149c5b07f4
kaiomendes15 <kakacnmendes@gmail.com>
2026-01-26 18:06:12 -0300

    atualiza o metodo atualizarTransacao para utilizar o mapper, o metodo interno de transacao para atualizar dados e o Dirty Checking do Hibernate

--- src/main/java/br/com/mywallet/app/domain/service/TransacaoService.java
+++ src/main/java/br/com/mywallet/app/domain/service/TransacaoService.java
@@ -9,13 +9,12 @@ import br.com.mywallet.app.domain.model.Usuario.Usuario;
 import br.com.mywallet.app.repository.CategoriaRepository;
 import br.com.mywallet.app.repository.TransacaoRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import static br.com.mywallet.app.domain.model.Transacao.TransacaoMapper.transacaoParaDto;
 
 @Service
 @RequiredArgsConstructor
@@ -76,5 +75,27 @@ public class TransacaoService {
         }
 
         repository.deleteTransacaoById(transacaoId);
+    }
+
+    @Transactional
+    public TransacaoResponseDTO atualizarTransacao(Usuario usuarioLogado, Long transacaoId,
+                                                   TransacaoRequestDTO updatedData) {
+
+        Transacao transacao = repository.findTransacaoByIdAndUsuarioId(transacaoId, usuarioLogado.getId()).orElseThrow(() -> new ResourceNotFoundException("Transação com id " + transacaoId + " não foi encontrada para o usuário " + usuarioLogado.getEmail() + "."));;
+
+        Categoria categoria = categoriaRepository.findCategoriaByIdAndUsuarioId(updatedData.categoriaId(), usuarioLogado.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("Categoria com id " + updatedData.categoriaId() + " não foi encontrada para o usuário " + usuarioLogado.getEmail() + "."));
+
+        transacao.atualizarDados(updatedData, categoria);
+
+        // Transacao transacaoAtualizada = repository.save(transacao);
+        // GEMINI:
+        // Você anotou o metodo com @Transactional. Quando você busca um objeto pelo repositório dentro de uma
+        // transação (findTransacaoById...), esse objeto entra em um estado chamado Managed (Gerenciado). O
+        // Hibernate/JPA fica "vigiando" esse objeto. Se você altera qualquer atributo dele (setDescricao, setValor, etc.),
+        // o Hibernate percebe essa mudança automaticamente. Quando o metodo termina (e o commit da transação acontece),
+        // o Hibernate gera o UPDATE no banco sozinho.
+
+        return transacaoParaDto(transacao);
     }
 }
