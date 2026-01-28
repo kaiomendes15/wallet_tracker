63ab8cb3a4b7481a9fde4d6f0c5918c7926e13b9
kaiomendes15 <kakacnmendes@gmail.com>
2026-01-28 19:43:53 -0300

    adiciona o endpoint para receber os dados do dashboard

--- src/main/java/br/com/mywallet/app/controller/DashboardController.java
+++ src/main/java/br/com/mywallet/app/controller/DashboardController.java
@@ -1,4 +1,35 @@
 package br.com.mywallet.app.controller;
 
+import br.com.mywallet.app.domain.model.Dashboard.DashboardResponseDTO;
+import br.com.mywallet.app.domain.model.Usuario.Usuario;
+import br.com.mywallet.app.domain.service.DashboardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/dashboard")
+@RequiredArgsConstructor
 public class DashboardController {
+
+    private final DashboardService dashboardService;
+
+    @GetMapping
+    public ResponseEntity<DashboardResponseDTO> receberDashboard(
+            Authentication authentication,
+            @RequestParam(required = false) LocalDate inicio,
+            @RequestParam(required = false) LocalDate fim
+    ) {
+        Usuario usuario = (Usuario) authentication.getPrincipal();
+
+        DashboardResponseDTO dados = dashboardService.buscarDadosDashboard(usuario, inicio, fim);
+
+        return ResponseEntity.ok(dados);
+    }
 }
