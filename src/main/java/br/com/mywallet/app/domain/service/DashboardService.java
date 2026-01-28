379e503eccc7602ec4d868cbb9e050ab983571d0
kaiomendes15 <kakacnmendes@gmail.com>
2026-01-28 20:03:34 -0300

    adiciona a funcao para buscar os gastos das top 5 categorias

--- src/main/java/br/com/mywallet/app/domain/service/DashboardService.java
+++ src/main/java/br/com/mywallet/app/domain/service/DashboardService.java
@@ -2,6 +2,7 @@ package br.com.mywallet.app.domain.service;
 
 import br.com.mywallet.app.domain.exceptions.RegraDeNegocioException;
 import br.com.mywallet.app.domain.model.Dashboard.DashboardResponseDTO;
+import br.com.mywallet.app.domain.model.Dashboard.GastosPorCategoriaDTO;
 import br.com.mywallet.app.domain.model.Usuario.Usuario;
 import br.com.mywallet.app.repository.TransacaoRepository;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -31,4 +34,26 @@ public class DashboardService {
 
         return transacaoRepository.buscarDashboard(usuario.getId(), inicio, fim);
     }
+
+    public List<GastosPorCategoriaDTO> buscarGastosPorCategoria(Usuario usuario, LocalDate inicio, LocalDate fim) {
+
+        if (inicio == null || fim == null) {
+            LocalDate hoje = LocalDate.now();
+            inicio = hoje.withDayOfMonth(1); // Dia 1 do mês atual
+            fim = hoje.withDayOfMonth(hoje.lengthOfMonth()); // Último dia do mês
+        }
+
+        if (inicio.isAfter(fim)) {
+            throw new RegraDeNegocioException("A data de início não pode ser posterior à data fim.");
+        }
+
+        List<GastosPorCategoriaDTO> listaCompleta = transacaoRepository.buscarGastosPorCategoria(usuario.getId(), inicio, fim);
+
+        // pega so os 5 primeiros
+        return listaCompleta.stream()
+                .limit(5)
+                .collect(Collectors.toList());
+    }
+
+
 }
