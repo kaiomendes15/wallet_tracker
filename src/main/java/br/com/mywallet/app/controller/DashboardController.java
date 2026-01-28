f33988acff2b0d7d3200985f84dd918e33fef2d3
kaiomendes15 <kakacnmendes@gmail.com>
2026-01-28 20:04:45 -0300

    adiciona o endpoint para receber os gastos por categoria

--- src/main/java/br/com/mywallet/app/controller/DashboardController.java
+++ src/main/java/br/com/mywallet/app/controller/DashboardController.java
@@ -1,6 +1,7 @@
 package br.com.mywallet.app.controller;
 
 import br.com.mywallet.app.domain.model.Dashboard.DashboardResponseDTO;
+import br.com.mywallet.app.domain.model.Dashboard.GastosPorCategoriaDTO;
 import br.com.mywallet.app.domain.model.Usuario.Usuario;
 import br.com.mywallet.app.domain.service.DashboardService;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/dashboard")
@@ -31,5 +33,16 @@ public class DashboardController {
         DashboardResponseDTO dados = dashboardService.buscarDadosDashboard(usuario, inicio, fim);
 
         return ResponseEntity.ok(dados);
+    }
+
+    @GetMapping("/gastos-por-categoria")
+    public ResponseEntity<List<GastosPorCategoriaDTO>> getGastosPorCategoria(
+            Authentication authentication,
+            @RequestParam(required = false) LocalDate inicio,
+            @RequestParam(required = false) LocalDate fim
+    ) {
+        Usuario usuario = (Usuario) authentication.getPrincipal();
+        var resultado = dashboardService.buscarGastosPorCategoria(usuario, inicio, fim);
+        return ResponseEntity.ok(resultado);
     }
 }
