a9d012d85dabbaf419eeeb732576c4cf52c057d2
kaiomendes15 <kakacnmendes@gmail.com>
2026-01-28 19:25:25 -0300

    altera os tipos dos atributos do dashboard para double

--- src/main/java/br/com/mywallet/app/domain/model/Dashboard/DashboardResponseDTO.java
+++ src/main/java/br/com/mywallet/app/domain/model/Dashboard/DashboardResponseDTO.java
@@ -3,14 +3,14 @@ package br.com.mywallet.app.domain.model.Dashboard;
 import java.math.BigDecimal;
 
 public record DashboardResponseDTO(
-        BigDecimal totalReceitas,
-        BigDecimal totalDespesas,
-        BigDecimal saldo
+        Double totalReceitas,
+        Double totalDespesas,
+        Double saldo
 ) {
     // Construtor canônico para garantir que não venha nulo do banco
-    public DashboardResponseDTO(BigDecimal totalReceitas, BigDecimal totalDespesas, BigDecimal saldo) {
-        this.totalReceitas = totalReceitas != null ? totalReceitas : BigDecimal.ZERO;
-        this.totalDespesas = totalDespesas != null ? totalDespesas : BigDecimal.ZERO;
-        this.saldo = saldo != null ? saldo : BigDecimal.ZERO;
+    public DashboardResponseDTO(Double totalReceitas, Double totalDespesas, Double saldo) {
+        this.totalReceitas = totalReceitas != null ? totalReceitas : 0.0;
+        this.totalDespesas = totalDespesas != null ? totalDespesas : 0.0;
+        this.saldo = saldo != null ? saldo : 0.0;
     }
 }
