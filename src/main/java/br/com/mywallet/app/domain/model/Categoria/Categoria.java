a47851489e8d6ba762604bc18bc33918d67d3e94
kaiomendes15 <kakacnmendes@gmail.com>
2026-01-31 14:03:54 -0300

    adiciona o metodo para criar as categorias padroes do sistema

--- src/main/java/br/com/mywallet/app/domain/model/Categoria/Categoria.java
+++ src/main/java/br/com/mywallet/app/domain/model/Categoria/Categoria.java
@@ -5,11 +5,13 @@ import br.com.mywallet.app.domain.model.Usuario.Usuario;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Categoria {
--- src/main/java/br/com/mywallet/app/domain/service/CategoriaService.java
+++ src/main/java/br/com/mywallet/app/domain/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package br.com.mywallet.app.domain.service;
 
+import br.com.mywallet.app.domain.enums.TipoTransacao;
 import br.com.mywallet.app.domain.model.Categoria.Categoria;
 import br.com.mywallet.app.domain.model.Categoria.CategoriaRequestDTO;
 import br.com.mywallet.app.domain.model.Categoria.CategoriaResponseDTO;
@@ -11,19 +12,20 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CategoriaService {
 
-    private final CategoriaRepository categoriarepository;
+    private final CategoriaRepository categoriaRepository;
 
     public List<CategoriaResponseDTO> getAllCategorias(Long usuarioId) {
-        List<Categoria> categorias = categoriarepository.findByUsuarioId(usuarioId);
+        List<Categoria> categorias = categoriaRepository.findByUsuarioId(usuarioId);
 
         return categorias.stream()
-                .map(cat -> new CategoriaResponseDTO(cat.getId(), cat.getTitulo()))
+                .map(cat -> new CategoriaResponseDTO(cat.getId(), cat.getTitulo(), cat.getTipo()))
                 .toList();
     }
 
@@ -35,14 +37,14 @@ public class CategoriaService {
         novaCategoria.setTitulo(data.titulo());
         novaCategoria.setUsuario(usuarioLogado);
 
-        Categoria categoriaSalva = categoriarepository.save(novaCategoria);
+        Categoria categoriaSalva = categoriaRepository.save(novaCategoria);
 
-        return new CategoriaResponseDTO(categoriaSalva.getId(), categoriaSalva.getTitulo());
+        return new CategoriaResponseDTO(categoriaSalva.getId(), categoriaSalva.getTitulo(), categoriaSalva.getTipo());
     }
 
     public CategoriaResponseDTO atualizarCategoria(CategoriaRequestDTO updatedData, Usuario usuarioLogado, Long categoriaId) {
 
-        Categoria categoria = categoriarepository.findCategoriaByIdAndUsuarioId(categoriaId, usuarioLogado.getId())
+        Categoria categoria = categoriaRepository.findCategoriaByIdAndUsuarioId(categoriaId, usuarioLogado.getId())
                 .orElseThrow(() -> new ResourceNotFoundException("Categoria com id " + categoriaId + " não foi encontrada para o usuário " + usuarioLogado.getEmail() + "."));
 
         if (!categoria.getTitulo().equalsIgnoreCase(updatedData.titulo())) {
@@ -50,24 +52,47 @@ public class CategoriaService {
         }
 
         categoria.setTitulo(updatedData.titulo());
-        Categoria categoriaAtualizada = categoriarepository.save(categoria);
+        Categoria categoriaAtualizada = categoriaRepository.save(categoria);
 
-        return new CategoriaResponseDTO(categoriaAtualizada.getId(), categoriaAtualizada.getTitulo());
+        return new CategoriaResponseDTO(categoriaAtualizada.getId(), categoriaAtualizada.getTitulo(), categoriaAtualizada.getTipo());
     }
 
     @Transactional
     public void excluirCategoria(Long categoriaId, Usuario usuarioLogado) {
-        if (!categoriarepository.existsByIdAndUsuarioId(categoriaId, usuarioLogado.getId())) {
+        if (!categoriaRepository.existsByIdAndUsuarioId(categoriaId, usuarioLogado.getId())) {
             throw new ResourceNotFoundException("Categoria com id " + categoriaId + " não foi encontrada para o usuário " + usuarioLogado.getEmail() + ".");
         }
 
-        categoriarepository.deleteCategoriaById(categoriaId);
+        categoriaRepository.deleteCategoriaById(categoriaId);
     }
 
     // Métodos auxiliares
     private void validarNomeDuplicado(String titulo, Long usuarioId) {
-        if (categoriarepository.existsByTituloAndUsuarioId(titulo, usuarioId)) {
+        if (categoriaRepository.existsByTituloAndUsuarioId(titulo, usuarioId)) {
             throw new RegraDeNegocioException("Você já possui uma categoria com este nome.");
         }
+    }
+
+    public void criarCategoriasPadrao(Usuario usuario) {
+        List<Categoria> padroes = new ArrayList<>();
+
+        // Receitas
+        padroes.add(criar("Salário", TipoTransacao.RECEITA, usuario));
+        padroes.add(criar("Investimentos", TipoTransacao.RECEITA, usuario));
+
+        // Despesas
+        padroes.add(criar("Alimentação", TipoTransacao.DESPESA, usuario));
+        padroes.add(criar("Lazer", TipoTransacao.DESPESA, usuario));
+        padroes.add(criar("Moradia", TipoTransacao.DESPESA, usuario));
+
+        categoriaRepository.saveAll(padroes);
+    }
+
+    private Categoria criar(String titulo, TipoTransacao tipo, Usuario usuario) {
+        return Categoria.builder()
+                .titulo(titulo)
+                .tipo(tipo)
+                .usuario(usuario)
+                .build();
     }
 }
